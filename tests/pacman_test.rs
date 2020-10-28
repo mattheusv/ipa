7c4ecc783c41b7cd4267905150875db59d36c3f6
Matheus Alcantara <matheusssilv97@gmail.com>
2020-10-28 19:50:37 -0300

    ci: Create test file

--- tests/pacman_test.rs
+++ tests/pacman_test.rs
@@ -1,9 +1,14 @@
 use ipa::{config, pacman, runner};
 use std::env::current_dir;
+use std::fs::File;
+use std::io::prelude::*;
 use std::path::Path;
 
 #[test]
 fn test_setup_pacman() {
+    let mut file = File::create("/tmp/src.txt").unwrap();
+    file.write_all(b"Hello, ipa!").unwrap();
+
     let config = config::Config::load(
         current_dir()
             .unwrap()
@@ -17,7 +22,7 @@ fn test_setup_pacman() {
     assert!(ipa.setup().is_ok());
     assert!(pacman.is_installed("fzf").unwrap());
     assert!(pacman.is_installed("tmux").unwrap());
-    assert!(Path::new("/tmp/bashrc.tmp").exists());
+    assert!(Path::new("/tmp/dst.txt").exists());
     assert!(Path::new("/tmp/tmux-plugins/").exists());
     assert!(Path::new("/tmp/i3blocks/").exists());
 }
